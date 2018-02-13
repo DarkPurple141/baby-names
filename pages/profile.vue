79f90c013b33305b4e2f2f55fe2367bb5a0beea6
Alex Hinds <alex.hinds141@gmail.com>
2018-02-13 20:09:24 +1100

    added social share

--- pages/profile.vue
+++ pages/profile.vue
@@ -113,7 +113,6 @@ export default {
    },
 
    beforeMount() {
-      /*
       if (!(this.$route.query.u &&
           this.$route.query.u.length === 24)) {
          this.$router.push('/')
@@ -125,7 +124,7 @@ export default {
             data.lists.forEach(
                list => this.lists.push(list))
          })
-         */
+
    }
 }
 </script>
